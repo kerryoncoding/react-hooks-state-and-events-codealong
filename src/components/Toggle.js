521c63414063ed82f74abd4a7f52289913893090
Kerry Guarino <kerry.guarino@gmail.com>
2023-06-26 17:20:12 -0400

    toggles button red

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 function Toggle() {
-  return <button>OFF</button>;
+
+// const [text, setText] = useState("OFF")
+
+// function changeButton(){
+//   if (text === "OFF"){
+//     setText("ON")
+//   } else {
+//     setText("OFF")
+//   }
+// }
+
+const [isOn, setIsOn] = useState(false);
+
+function handleClick() {
+  setIsOn((isOn) => !isOn);
+}
+
+const color = isOn ? "red" : "white";
+
+ return <button style={{background: color}}onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>
+
+  // return <button onClick={changeButton}>{text}</button>;
 }
 
 export default Toggle;
